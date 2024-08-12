7f88326d1045f6627c41520044efc0f11ae204eb
Giovanni Molina <giovanni.m.05@gmail.com>
2024-08-12 21:48:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// https://jsonplaceholder.typicode.com/photos
+const obtenerDatos = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/photos';
+    try{
+        const respuesta = await fetch(url);
+        const datos = await respuesta.json();
+        // console.log(datos);
+
+        // forEach documentacion
+        // array1.forEach((element) => console.log(element));
+        datos.forEach((element) => {
+            if(element.id < 21){
+                console.log(element);
+            }
+        });
+    }catch(error){
+        console.error(error);
+    }
+
+}
+
+obtenerDatos();
+
+const mensaje = () => {
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve('Información Enviada');
+        },3000);
+    });
+}
+
+const getMensaje = async () => {
+    const resultado = await mensaje();
+    console.log(resultado);
+}
+
+getMensaje();
